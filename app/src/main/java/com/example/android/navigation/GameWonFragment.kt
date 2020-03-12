0c7f66e4e231fcec7e63292ba1f13407ebe47601
mattiasback <mattias.j.back@gmail.com>
2020-03-12 22:57:26 +0100

    Share via IntentBuilder instead

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -48,15 +49,24 @@ class GameWonFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.winner_menu, menu)
+        //check if the activity resolves
+        if(null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            menu.findItem(R.id.share).setVisible(false)
+        }
     }
 
     private fun getShareIntent() : Intent {
         var args = GameWonFragmentArgs.fromBundle(arguments!!)
-        val shareIntent = Intent(Intent.ACTION_SEND)
-        shareIntent.setType("text/plain")
-                .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect,
-                        args.numQuestions))
-        return shareIntent
+//        val shareIntent = Intent(Intent.ACTION_SEND)
+//        shareIntent.setType("text/plain")
+//                .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect,
+//                        args.numQuestions))
+//        return shareIntent
+
+        return ShareCompat.IntentBuilder.from(activity!!)
+                .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+                .setType("text/plain")
+                .intent
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
